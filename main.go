6d413f9bb24510b8f03e096392293ded042626f5
Alexandr Lobov <bakamoridesu@gmail.com>
2023-12-26 00:04:27 +0300

    refined config structure, added config parser

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
@@ -8,20 +9,60 @@ import (
 	"time"
 )
 
-func main() {
-	// load config
+type arg struct {
+	Param   string `json:"param"`
+	Default string `json:"default"`
+}
 
+type flag struct {
+	Name    string `json:"name"`
+	Param   string `json:"param"`
+	Default string `json:"default"`
+}
+
+type config struct {
+	Command string `json:"command"`
+	Args    []arg  `json:"args"`
+	Flags   []flag `json:"flags"`
+}
+
+func (c config) String() string {
+	args := "args:\n"
+	for _, arg := range c.Args {
+		args += fmt.Sprintf("\tparam:%s, defalt:%s", arg.Param, arg.Default)
+	}
+	flags := "flags:\n"
+	for _, flag := range c.Flags {
+		flags += fmt.Sprintf("\tname:%s, param:%s, defalt:%s", flag.Name, flag.Param, flag.Default)
+	}
+
+	return fmt.Sprintf("command:%s\n%s\n%s", c.Command, args, flags)
+}
+
+func run() error {
+	cfg := &config{}
+	cfgFile, err := os.Open("config.json")
+	if err != nil {
+		return err
+	}
+	defer cfgFile.Close()
+
+	if err := json.NewDecoder(cfgFile).Decode(cfg); err != nil {
+		return err
+	}
+
+	fmt.Println(cfg)
 	// check if template found
+
+	cName, cParams := GetOpen()
+	cParams = append(cParams, "http://127.0.0.1:8080")
+	cPath, err := exec.LookPath(cName)
+	if err != nil {
+		return err
+	}
 
 	go func() {
 		time.Sleep(2 * time.Second)
-		cName, cParams := GetOpen()
-		cParams = append(cParams, "http://127.0.0.1:8080")
-		cPath, err := exec.LookPath(cName)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(1)
-		}
 		exec.Command(cPath, cParams...).Run()
 	}()
 
@@ -38,8 +79,15 @@ func main() {
 		WriteTimeout: 10 * time.Second,
 	}
 	if err := s.ListenAndServe(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func main() {
+	if err := run(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
-
 }
